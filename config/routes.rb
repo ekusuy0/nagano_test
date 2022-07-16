5d9ad51a1d4f2b668d4b509981694c0f51a6dd00
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-16 01:25:01 +0000

    [Update] 商品登録ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
+
+  root to: "public/homes#top"
 
   #顧客用
   #URL /customers/sign_in ...
@@ -7,14 +12,13 @@ Rails.application.routes.draw do
     registrations: 'public/registrations'
   }
 
-  root to: "public/homes#top"
-
   namespace :public do
     resources :customers, only: [:show, :edit, :upadate]
+    resources :items, only: [:index, :show]
     #退会確認画面
-    get 'public/customers/:id/out_check' => 'customers#out_check', as: 'out_check'
+    get 'customers/:id/out_check' => 'customers#out_check', as: 'out_check'
     #論理削除用のルーティング
-    patch '/customers/:id' => 'customers#out', as: 'out'
+    patch 'customers/:id' => 'customers#out', as: 'out'
   end
 
 
@@ -25,5 +29,9 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
+
+  namespace :admin do
+    resources :items, except: [:destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
