131ab60628cf4252af668930506dabd8461a2b3e
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-21 15:48:11 +0000

    [Update] 注文履歴詳細画面Joinの使用.ver

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items
     @total = 0
   end
 
@@ -28,14 +28,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    cart_item = CartItem.find_by(item_id: @cart_item.item_id)
-    if cart_item.present?
-      cart_item.update(quantity: cart_item.quantity.to_i + @cart_item.quantity.to_i)
-    else
-       @cart_item.save
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        cart_item.update(quantity: cart_item.quantity.to_i + @cart_item.quantity.to_i)
+        @cart_item.delete
+      end
     end
+    @cart_item.save
     redirect_to public_cart_items_path
   end
 
